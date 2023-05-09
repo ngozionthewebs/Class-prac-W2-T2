5fa6197746878155f61759603638df43067c614d
ngozionthewebs <231285@virtualwindow.co.za>
2023-05-09 20:31:43 +0200

    class prac done

---
+++ js/index.js
@@ -0,0 +1,26 @@
+console.log("Hello World")
+
+var firstName = "Ngozi"
+let surname = "Ojukwu"
+let age = "20"
+const subject = "Development"
+
+let message = "Hello " + firstName + " " + surname + " ,You are " + age + "  years old, and your subject is " + subject + "."
+
+console.log(message)
+
+let tim = 13;
+let jane = 23;
+let john = 10;
+
+tim = tim + 10;
+
+let teamTotal = tim + jane;
+let victory = john / 2;
+let total = teamTotal + victory;
+
+
+
+let answer = "Tim and Jane now have " + total + " marbles!"
+
+console.log(answer)
